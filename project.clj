dbacf16db3187a9d0be72411cffc4b167119c178
Christian Berg <berg.christian@gmail.com>
2012-01-20 14:12:42 +0100

    Setup midje for testing.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject herjel "0.1.0-SNAPSHOT"
   :description "A ray tracer in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [lein-midje "1.0.7"]])
--- test/herjel/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns herjel.test.core
-  (:use [herjel.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
