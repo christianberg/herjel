a94dd14678bd870acb67463397e7219d9853ebec
Christian Berg <berg.christian@gmail.com>
2012-02-02 01:13:54 +0100

    Implement camera ray generation.

--- src/herjel/core.clj
+++ src/herjel/core.clj
@@ -1,22 +1,27 @@
 (ns herjel.core
   (:use midje.sweet))
 
-(unfinished )
-
-(def camera-defaults
-  {:origin [0 0 0]
-   :viewport [[2 -1.5 1] [2 1.5 -1]]
-   :resolution [[320 240]]})
-
-(defn camera-rays
-  "Returns a sequence of rays corresponding to pixels in a rectangular image."
-  []
-  )
-
-(fact "")
+(defn roughlies [expected]
+  (fn [actual]
+    (every? identity (map #((roughly %1) %2) expected actual))))
 
 (def black [0 0 0])
 (def white [1 1 1])
+
+(defn vec+ [& vs]
+  (apply map + vs))
+
+(fact "vector addition"
+  (vec+ [1 2 3]) => [1 2 3]
+  (vec+ [1 2 3] [4 5 6]) => [5 7 9]
+  (vec+ [0 0 1] [0 1 0] [1 0 0]) => [1 1 1])
+
+(defn scale [scalar vector]
+  (map (partial * scalar) vector))
+
+(fact "scaling a vector"
+  (scale 2 [3 5 9]) => [6 10 18]
+  (scale -0.5 [0 20 -8.4]) => [0.0 -10.0 4.2])
 
 (defn vecdot [v1 v2]
   (reduce + (map * v1 v2)))
@@ -26,11 +31,60 @@
   (vecdot [1 0 0] [1 0 0]) => 1
   (vecdot [1 2 3] [4 5 6]) => 32)
 
+(defn veclen [v]
+  (Math/sqrt (vecdot v v)))
+
+(fact "vector length"
+  (veclen [1 0 0]) => 1.0
+  (veclen [3 4 0]) => 5.0)
+
 (defn vec- [& vs]
   (apply (partial map -) vs))
 
 (fact "vector difference"
   (vec- [4 6 3] [2 9 1]) => [2 -3 2])
+
+(defn norm [v]
+  (scale (/ (veclen v)) v))
+
+(fact "normalize vector"
+  (norm [1.0 0.0 0.0]) => [1.0 0.0 0.0]
+  (norm [0 2 0]) => [0.0 1.0 0.0]
+  (norm [3 0 4]) => (roughlies [0.6 0.0 0.8]))
+
+(def camera-defaults
+  {:origin [0 0 0]
+   :viewport [[-2 1.5 2] [2 1.5 2] [2 -1.5 2]]
+   :x-resolution 320})
+
+(defn camera-rays
+  "Returns a sequence of rays corresponding to pixels in a rectangular image."
+  ([] (camera-rays camera-defaults))
+  ([{:keys [origin viewport x-resolution]}]
+     (let [[upper-left upper-right lower-right] viewport
+           width (vec- upper-right upper-left)
+           height (vec- lower-right upper-right)
+           ratio (/ (veclen width) (veclen height))
+           y-resolution (int (/ x-resolution ratio))
+           dx (scale (/ x-resolution) width)
+           dy (scale (/ y-resolution) height)
+           start (vec+ upper-left (scale 0.5 dx) (scale 0.5 dy))]
+       (for [y (range y-resolution)
+             x (range x-resolution)]
+         [origin (norm (vec+ start (scale x dx) (scale y dy)))]))))
+
+(fact "camera ray tests"
+  (let [options {:origin [0 0 0]
+                 :viewport [[-1 1 1] [1 1 1] [1 -1 1]]}
+        a (/ 0.5 (Math/sqrt 1.5))
+        b (/ -0.5 (Math/sqrt 1.5))
+        c (/ 1 (Math/sqrt 1.5))]
+    (camera-rays (assoc options :x-resolution 1)) => [[[0 0 0] [0.0 0.0 1.0]]]
+    (camera-rays (assoc options :x-resolution 2)) => [[[0 0 0] [b a c]]
+                                                      [[0 0 0] [a a c]]
+                                                      [[0 0 0] [b b c]]
+                                                      [[0 0 0] [a b c]]]
+    (count (camera-rays)) => (* 320 240)))
 
 (defn sphere [center radius]
   (fn [origin direction]
