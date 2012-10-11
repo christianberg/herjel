b4d90ce7d984ca43c7bbaf0514968235ac9df7d7
Christian Berg <berg.christian@gmail.com>
2012-10-11 15:15:41 +0200

    Update to Clojure 1.4.0 and current Midje version.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject herjel "0.1.0-SNAPSHOT"
   :description "A ray tracer in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3.1" :exclusions [org.clojure/clojure]]
-                     [lein-midje "1.0.7"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
