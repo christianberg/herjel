66f491dd989b1c640e9f667ae3e41e46f23339be
Christian Berg <berg.christian@gmail.com>
2013-03-16 23:51:15 +0100

    Upgrade dependencies: Clojure 1.5 and latest Midje

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject herjel "0.1.0-SNAPSHOT"
   :description "A ray tracer in Clojure"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :plugins [[lein-midje "3.0.0"]])
